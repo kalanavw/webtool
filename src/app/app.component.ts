1141ff581ac2767e991068ead62725e13c784905
Kalana Weerarathne <kalanaweerarathna@hotmail.com>
2023-06-12 10:39:48 +0000

    customize header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webtool';
+//image crop, convert the extension, parse into pdf, pdf into images
+// image in to base64, base64 into image
+//base64 generation in every language
+//interview question
+
+
+// QR generator
 }
+
+
+ // npm run ng serve -- --host=0.0.0.0 --disable-host-check
